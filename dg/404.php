15011473d3cde01da46da5939b1adbfc842a5450
Biddrup Kumar Mallick <biddrup3@gmail.com>
2017-05-15 14:40:11 +0600

    Test
    mklink /j and rest are \. They are working like what I was thinking

--- dg/404.php
+++ dg/404.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
 <h2>This is a 404 page</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint voluptatibus blanditiis exercitationem provident explicabo minima quasi dolores, labore adipisci, ea nostrum unde delectus reiciendis sequi est omnis aliquid perspiciatis sit.</p>
 <img src="http://placehold.it/500x500" alt="">
